b7807043d3486db0613d5a83f661843d0d691ef8
mthansen <maxwell.hansen@cern.ch>
2022-12-14 13:06:09 +0000

    docs: another expt

--- ampyl/ampyl.py
+++ ampyl/ampyl.py
@@ -646,7 +646,7 @@ class FlavorChannelSpace:
 
         In the case of a qcd channel space len(sc_compact[0].T) is 10. Each
         row is populated as follows:::
-            .. code-block:: python
+        .. code-block:: python
 
             [3.0, mass1, mass2, mass3, spin1, spin2, spin3, isospin_flavor,
             isospin_value, sub_isospin]
