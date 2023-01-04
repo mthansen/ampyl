43e8a675dac56b64536e8fe984494a7c78f44361
mthansen <maxwell.hansen@cern.ch>
2023-01-04 18:52:07 +0000

    docs: undo html theme change

--- doc/conf.py
+++ doc/conf.py
@@ -66,14 +66,13 @@ autosummary_generate = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "dotted"
-# html_theme = 'jupyter'
-# html_theme_path = [p+'/doc']
+html_theme = 'jupyter'
+html_theme_path = [p+'/doc']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['jupyter/static']
+html_static_path = ['jupyter/static']
 
 #### Tweaks by Mattia
 
