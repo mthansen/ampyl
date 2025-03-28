1f4209cc66274119aba4dc3a3530054653f116b1
mthansen <maxwell.hansen@cern.ch>
2025-03-28 17:00:51 +0000

    fix: correct argument passing a second time

--- ampyl/ampyl.py
+++ ampyl/ampyl.py
@@ -202,7 +202,7 @@ class K:
             Pvec = TWOPI*nP/L
             PmkSQ_arr = ((Pvec-kvec_arr)**2).sum(axis=1)
             threshold = m2+m3
-            zero_support_point = self._get_zero_support_point(self, threshold)
+            zero_support_point = self._get_zero_support_point(threshold)
             mask = (E-omk_arr)**2-PmkSQ_arr > zero_support_point
             if self.qcis.verbosity >= 2:
                 print('mask =')
