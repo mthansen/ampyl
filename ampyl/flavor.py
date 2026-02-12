34dc9c7e199c1b4ad9d0b27be91e40938363a841
mthansen <maxwell.hansen@cern.ch>
2026-02-12 00:35:53 +0000

    feat: add __str__ method to FlavorChannelSpace class for string representation

--- ampyl/flavor.py
+++ ampyl/flavor.py
@@ -1172,3 +1172,14 @@ class FlavorChannelSpace:
                     collective_index_i += 1
         g_templates_ell_specific_db = np.array(g_templates_ell_specific_db)
         return g_templates_ell_specific_db
+
+    def __str__(self):
+        """Return a string representation of the FlavorChannelSpace object."""
+        flavor_channel_space_str = "FlavorChannelSpace with the following "\
+            + "SpectatorChannels:\n"
+        for sc in self.sc_list_sorted:
+            flavor_channel_space_str += "    "
+            flavor_channel_space_str += sc.__str__().replace("\n    ",
+                                                             "\n        ")[:-1]
+            flavor_channel_space_str += ",\n"
+        return flavor_channel_space_str[:-2]+"."
