5f0700ac126066e2c1f572cc6986f594d04bbc86
mthansen <maxwell.hansen@cern.ch>
2026-02-12 00:35:42 +0000

    feat: add _populate_g_templates_db method to FlavorChannelSpace class for generating g templates database

--- ampyl/flavor.py
+++ ampyl/flavor.py
@@ -1136,3 +1136,39 @@ class FlavorChannelSpace:
                 pass
         return g_templates_ell_specific_db
 
+    def _populate_g_templates_db(self):
+        g_templates_ell_specific_db = []
+        collective_index_i = 0
+        for slice_index_i in range(len(self.slices_by_three_masses)):
+            slice_i = self.slices_by_three_masses[slice_index_i]
+            three_mass_slice_i = self.sc_list_sorted[slice_i[0]:slice_i[1]]
+            for sc_index_i in range(len(three_mass_slice_i)):
+                sc_i = three_mass_slice_i[sc_index_i]
+                for ell_i in sc_i.ell_set:
+                    collective_index_j = 0
+                    for slice_index_j in range(len(
+                                self.slices_by_three_masses)):
+                        slice_j = self.slices_by_three_masses[
+                                slice_index_j]
+                        three_mass_slice_j = self.sc_list_sorted[
+                                slice_j[0]:slice_j[1]]
+                        g_template_ij = self.g_templates[slice_index_i][
+                                slice_index_j]
+                        for sc_index_j in range(len(three_mass_slice_j)):
+                            sc_j = three_mass_slice_j[sc_index_j]
+                            for ell_j in sc_j.ell_set:
+                                g_templates_ell_specific_db.append(
+                                        np.array(
+                                            [slice_index_i,
+                                             slice_index_j,
+                                             ell_i, ell_j,
+                                             np.array([[g_template_ij[
+                                                 sc_index_i][sc_index_j]]]),
+                                             sc_index_i, sc_index_j,
+                                             collective_index_i,
+                                             collective_index_j],
+                                            dtype=object))
+                                collective_index_j += 1
+                    collective_index_i += 1
+        g_templates_ell_specific_db = np.array(g_templates_ell_specific_db)
+        return g_templates_ell_specific_db
