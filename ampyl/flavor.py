afa42f03c84af888beeb6ac500c07d7c92d603d2
mthansen <maxwell.hansen@cern.ch>
2026-02-24 11:29:02 +0000

    feat: update g template methods in FlavorChannelSpace to use flavor_ope_utils

--- ampyl/flavor.py
+++ ampyl/flavor.py
@@ -701,8 +701,8 @@ class FlavorChannelSpace:
 
     def update_g_templates(self):
         """Update the g templates."""
-        flavor_utils.build_g_templates(self)
-        flavor_utils.build_g_templates_ell_specific(self)
+        flavor_ope_utils.build_g_templates(self)
+        flavor_ope_utils.build_g_templates_ell_specific(self)
         if self.verbosity >= 2:
             self.print_summary()
 
