eda043378e9c171f9dc6070ec2e24f1890e5ef7d
mthansen <maxwell.hansen@cern.ch>
2026-04-30 13:34:54 +0100

    refactor: simplify QC consistency check in _simple_try_at_fixed_L function

--- ampyl/fv_spectrum_utils.py
+++ ampyl/fv_spectrum_utils.py
@@ -332,7 +332,7 @@ def _simple_try_at_fixed_L(spectrum, E_bracket, L, qc_dict):
             spectrum.qc.get_value(root+EPSILON6, L, qc_dict)
         )
         qc_ratio = abs_qc_value_at_root / abs_qc_value_at_root_plus
-        if abs_qc_value_at_root < EPSILON10 and qc_ratio < EPSILON5:
+        if qc_ratio < EPSILON6:
             return root
         warnings.warn("Root was found but it failed the QC consistency "
                       "checks, returning NaN.")
