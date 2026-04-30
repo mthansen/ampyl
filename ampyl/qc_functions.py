e473dcd7c339c607126cbe31db815bc4379ed159
mthansen <maxwell.hansen@cern.ch>
2026-04-30 13:17:09 +0100

    feat: add qc_functions module with initial implementation and documentation

---
+++ ampyl/qc_functions.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created July 2022.
+
+@author: M.T. Hansen
+"""
+
+###############################################################################
+#
+# qc_functions.py
+#
+# MIT License
+# Copyright (c) 2022 Maxwell T. Hansen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
+
+import numpy as np
+from scipy.special import erfi
+from scipy.special import erf
+from scipy.linalg import block_diag
+from .kinematic_functions import H
+from .kinematic_functions import calY
+from .kinematic_functions import q_one_minus_H
+from .kinematic_functions import standard_boost
+from .kinematic_functions import standard_boost_array
+from .constants import PI
+from .constants import TWOPI
+from .constants import FOURPI2
+from .constants import R4PI
+from .constants import EPSILON15
+from .constants import QC_IMPL_DEFAULTS
+import functools
+import warnings
+warnings.simplefilter("once")
