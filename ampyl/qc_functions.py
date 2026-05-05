2837011151027038ca21069908d95aa5274b041f
mthansen <maxwell.hansen@cern.ch>
2026-05-05 11:36:26 +0100

    fix: add getG_array_two_tbks function to handle G blocks with different TBKS entries

--- ampyl/qc_functions.py
+++ ampyl/qc_functions.py
@@ -607,6 +607,64 @@ def getG_array(E, nP, L, m1, m2, m3,
         return np.zeros_like(full_mat_big)
     return YY*full_mat_big*H1_mat*H2_mat*g_rescale
 
+
+def getG_array_two_tbks(E, nP, L, m1, m2, m3,
+                        row_tbks_entry, col_tbks_entry,
+                        row_shell, col_shell,
+                        ell1, ell2,
+                        alpha, beta,
+                        qc_impl, three_scheme,
+                        g_rescale):
+    """Return a G block whose row and column use different TBKS entries."""
+    row_nvec_arr = row_tbks_entry.nvec_arr[row_shell[0]:row_shell[1]]
+    row_nvecSQ_arr = row_tbks_entry.nvecSQ_arr[row_shell[0]:row_shell[1]]
+    col_nvec_arr = col_tbks_entry.nvec_arr[col_shell[0]:col_shell[1]]
+    col_nvecSQ_arr = col_tbks_entry.nvecSQ_arr[col_shell[0]:col_shell[1]]
+
+    nrow = len(row_nvec_arr)
+    ncol = len(col_nvec_arr)
+    ntotal = nrow+ncol
+    row_slice = [0, nrow]
+    col_slice = [nrow, ntotal]
+
+    class _MixedTBKSEntry:
+        pass
+
+    mixed_tbks_entry = _MixedTBKSEntry()
+    mixed_tbks_entry.nvec_arr = np.concatenate(
+        (row_nvec_arr, col_nvec_arr), axis=0)
+    mixed_tbks_entry.nvecSQ_arr = np.concatenate(
+        (row_nvecSQ_arr, col_nvecSQ_arr), axis=0)
+    shape = (ntotal, ntotal, 3)
+    mixed_tbks_entry.n1vec_mat = np.zeros(shape, dtype=int)
+    mixed_tbks_entry.n2vec_mat = np.zeros(shape, dtype=int)
+    mixed_tbks_entry.n3vec_mat = np.zeros(shape, dtype=int)
+    mixed_tbks_entry.n1vecSQ_mat = np.zeros((ntotal, ntotal), dtype=int)
+    mixed_tbks_entry.n2vecSQ_mat = np.zeros((ntotal, ntotal), dtype=int)
+    mixed_tbks_entry.n3vecSQ_mat = np.zeros((ntotal, ntotal), dtype=int)
+
+    row_mat = np.repeat(row_nvec_arr[:, np.newaxis, :], ncol, axis=1)
+    col_mat = np.repeat(col_nvec_arr[np.newaxis, :, :], nrow, axis=0)
+    exchange_mat = nP-row_mat-col_mat
+
+    row_idx = slice(row_slice[0], row_slice[1])
+    col_idx = slice(col_slice[0], col_slice[1])
+    mixed_tbks_entry.n2vec_mat[row_idx, col_idx] = row_mat
+    mixed_tbks_entry.n1vec_mat[row_idx, col_idx] = col_mat
+    mixed_tbks_entry.n3vec_mat[row_idx, col_idx] = exchange_mat
+    mixed_tbks_entry.n2vecSQ_mat[row_idx, col_idx] = (
+        row_mat*row_mat).sum(axis=2)
+    mixed_tbks_entry.n1vecSQ_mat[row_idx, col_idx] = (
+        col_mat*col_mat).sum(axis=2)
+    mixed_tbks_entry.n3vecSQ_mat[row_idx, col_idx] = (
+        exchange_mat*exchange_mat).sum(axis=2)
+
+    return getG_array(E, nP, L, m1, m2, m3,
+                      mixed_tbks_entry, row_slice, col_slice,
+                      ell1, ell2, alpha, beta, qc_impl, three_scheme,
+                      g_rescale)
+
+
 def getG_array_prep_mat(E, nP, L, m1, m2, m3,
                         tbks_entry,
                         row_shell_index, col_shell_index,
