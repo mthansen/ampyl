b539c7597e19808c3c2e000e4004a063013a0a9b
mthansen <maxwell.hansen@cern.ch>
2026-05-05 12:34:14 +0100

    fix: enhance ThreeBodyInteractionScheme to improve mass handling and ESQmin calculations

--- ampyl/schemes.py
+++ ampyl/schemes.py
@@ -363,14 +363,19 @@ class ThreeBodyInteractionScheme:
         if scheme_data is None:
             scheme_data_by_channel = []
             for sc in fcs.sc_list_sorted:
+                mspec = sc.fc.masses[sc.indexing[0]]
                 m1 = sc.fc.masses[sc.indexing[1]]
                 m2 = sc.fc.masses[sc.indexing[2]]
                 m_max = max(m1, m2)
                 m_min = min(m1, m2)
-                delta_mSQ = m_max**2 - m_min**2
-                ESQmins.append(delta_mSQ)
-                alpha = (3.*m_max - 5.*m_min) / (m_max + m_min)
+                if np.isclose(m1, m2) and mspec < m1:
+                    ESQmin_tmp = 4.0*(m1**2-mspec**2)
+                    alpha = 3.0-4.0*mspec**2/m1**2
+                else:
+                    ESQmin_tmp = m_max**2 - m_min**2
+                    alpha = (3.*m_max - 5.*m_min) / (m_max + m_min)
                 beta = 0.
+                ESQmins.append(ESQmin_tmp)
                 scheme_data_by_channel.append([alpha, beta])
         else:
             scheme_data_by_channel = self._scheme_data_by_channel(
@@ -385,6 +390,7 @@ class ThreeBodyInteractionScheme:
         if ESQmin_input is not None:
             ESQmins = [ESQmin_input]*len(threshSQs)
         self.ESQmin = ESQmins[0]
+        self.ESQMIN = self.ESQmin
         self.ESQmins = ESQmins
 
         self._set_flavor_ellm_dim()
