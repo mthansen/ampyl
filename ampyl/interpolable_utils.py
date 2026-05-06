2c6309457e90ecbdfd3e15627521d65c64e4bc87
mthansen <maxwell.hansen@cern.ch>
2026-05-06 21:15:07 +0100

    refactor: optimize cob_matrix retrieval process for improved performance and clarity

--- ampyl/interpolable_utils.py
+++ ampyl/interpolable_utils.py
@@ -458,13 +458,15 @@ def _get_all_relevant_nvecSQs_list_bad_loop(interpolable, Emax, project, irrep,
                                     .get_value(E=Etmp, L=Ltmp,
                                                project=project,
                                                irrep=irrep)
-                                for cob_matrix in cob_matrix_list:
-                                    try:
+                                cob_matrix_key_list =\
+                                    interpolable.cob_matrix_key_lists.get(
+                                        irrep, [])
+                                cob_matrix = _get_cob_matrix_for_value(
+                                    interpolable, Etmp, Ltmp, cob_matrix_list,
+                                    cob_matrix_key_list)
+                                if cob_matrix is not None:
                                         matrix_tmp =\
-                                            (cob_matrix.T)@matrix_tmp\
-                                            @ cob_matrix
-                                    except ValueError:
-                                        pass
+                                        (cob_matrix.T)@matrix_tmp@cob_matrix
                                 interpolable_value = matrix_tmp[i][j]
                                 near_pole_mag = np.abs(interpolable_value)
                                 pole_found = (near_pole_mag > POLE_CUT)
