48d2f1b76f996cc96a80d122471828ea71802ddc
mthansen <maxwell.hansen@cern.ch>
2026-05-07 11:00:51 +0100

    feat: add tests for canonicalizing pole candidates and maintaining mass assignments

--- tests/test_cuts.py
+++ tests/test_cuts.py
@@ -36,6 +36,7 @@ Created April 2026.
 import unittest
 import numpy as np
 import ampyl
+from ampyl import interpolable_utils
 
 
 class TestPoleCandidates(unittest.TestCase):
@@ -233,6 +234,83 @@ class TestPoleCandidates(unittest.TestCase):
 
         self.assertEqual(all_nvecSQs_set, expected_nvecSQs)
         self.assertEqual(len(all_nvecSQs), len(all_nvecSQs_set))
+
+    def test_canonicalize_pole_candidate_keeps_mass_alignment(self):
+        """Sorting nvecSQs should apply the same permutation to masses."""
+        pole_nvecSQs, pole_masses = interpolable_utils\
+            ._canonicalize_pole_candidate(
+                [5, 1, 3],
+                [10.0, 20.0, 30.0],
+            )
+
+        self.assertEqual(pole_nvecSQs, (1, 3, 5))
+        self.assertEqual(pole_masses, (20.0, 30.0, 10.0))
+
+    def test_pole_textures_keep_distinct_mass_assignments(self):
+        """Equal nvecSQs with different masses should remain distinct poles."""
+        polefree_interp_data_list = [[[
+            [],
+            [],
+            [
+                [0, 0, [1, 2, 3], [1.0, 2.0, 3.0]],
+                [0, 0, [1, 2, 3], [3.0, 2.0, 1.0]],
+            ],
+        ]]]
+
+        pole_list, pole_mass_list, pole_textures_list, _ = \
+            interpolable_utils._get_pole_textures(
+                self.f,
+                [1],
+                polefree_interp_data_list,
+            )
+
+        self.assertEqual(pole_list[0].tolist(), [[1, 2, 3], [1, 2, 3]])
+        self.assertEqual(
+            pole_mass_list[0].tolist(),
+            [[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]],
+        )
+        self.assertEqual(
+            pole_textures_list[0].tolist(),
+            [[[1.0]], [[1.0]]],
+        )
+
+    def test_f_pole_candidates_include_each_three_mass_slice(self):
+        """F candidates should retain separate mass assignments by slice."""
+        pion = ampyl.flavor.Particle(mass=1.0, spin=0.0, flavor='pi')
+        kaon = ampyl.flavor.Particle(mass=2.0, spin=0.0, flavor='K')
+        eta = ampyl.flavor.Particle(mass=3.0, spin=0.0, flavor='eta')
+        fc = ampyl.flavor.FlavorChannel(3, particles=[kaon, pion, eta])
+        qcis = ampyl.spaces.QCIndexSpace(
+            fcs=ampyl.flavor.FlavorChannelSpace(fc_list=[fc]),
+            Emax=10.0,
+        )
+        f = ampyl.F(qcis=qcis)
+        nvecSQs_by_shell = self._wrap_entry(
+            [[2]],
+            [[4]],
+            [[6]],
+        )
+
+        pole_candidates = f._get_pole_candidates_for_detection(
+            nvecSQs_by_shell
+        )
+        pole_candidate_keys = {
+            (tuple(candidate[0]), tuple(candidate[1]))
+            for candidate in pole_candidates
+            if tuple(candidate[0]) == (2, 4, 6)
+        }
+
+        self.assertEqual(
+            pole_candidate_keys,
+            {
+                ((2, 4, 6), (2.0, 1.0, 3.0)),
+                ((2, 4, 6), (1.0, 2.0, 3.0)),
+                ((2, 4, 6), (2.0, 3.0, 1.0)),
+                ((2, 4, 6), (3.0, 1.0, 2.0)),
+                ((2, 4, 6), (1.0, 3.0, 2.0)),
+                ((2, 4, 6), (3.0, 2.0, 1.0)),
+            },
+        )
 
     def test_f_pole_candidates_unsupported_diagonal_shell(self):
         """Unsupported diagonal labels should not add implied F triples."""
